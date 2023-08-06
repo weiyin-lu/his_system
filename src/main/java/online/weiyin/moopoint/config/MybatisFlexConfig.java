18641781766b4414dd64acc4836cbef264a74f92
weiyin-lu <weiyin2002@outlook.com>
2023-08-06 17:16:25 +0800

    myabtis-flex配置类

---
+++ src/main/java/online/weiyin/moopoint/config/MybatisFlexConfig.java
@@ -0,0 +1,25 @@
+package online.weiyin.moopoint.config;
+
+import com.mybatisflex.core.audit.AuditManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisFlexConfig {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger("mybatis-flex-sql");
+
+
+    public MybatisFlexConfig() {
+        //开启审计功能
+        AuditManager.setAuditEnable(true);
+
+        //设置 SQL 审计收集器
+        AuditManager.setMessageCollector(auditMessage ->
+                logger.info("{},{}ms", auditMessage.getFullSql()
+                        , auditMessage.getElapsedTime())
+        );
+    }
+}
