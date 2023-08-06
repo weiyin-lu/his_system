943fe29b3c256aaf6686389e75bba56c99221c8a
weiyin-lu <weiyin2002@outlook.com>
2023-08-06 16:56:22 +0800

    mybatis-fex示例

---
+++ src/main/java/online/weiyin/moopoint/mapper/DoctorMapper.java
@@ -0,0 +1,9 @@
+package online.weiyin.moopoint.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import online.weiyin.moopoint.entity.Doctor;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DoctorMapper extends BaseMapper<Doctor> {
+}
