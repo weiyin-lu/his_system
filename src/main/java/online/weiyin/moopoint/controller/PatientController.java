bf7f678ac9efaf4a28a74e5b76efa74aa059a91d
chencccc <18341797332@163.com>
2023-08-07 18:24:57 +0800

    查询所有订单

--- src/main/java/online/weiyin/moopoint/controller/PatientController.java
+++ src/main/java/online/weiyin/moopoint/controller/PatientController.java
@@ -1,11 +1,37 @@
 package online.weiyin.moopoint.controller;
 
+import cn.hutool.json.JSONUtil;
+import online.weiyin.moopoint.common.Result;
+import online.weiyin.moopoint.entity.Patient;
+import online.weiyin.moopoint.service.impl.PatientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * @Classname PatientController
- * @Description TODO
+ * @Description 挂号管理业务
  * @Version 1.0.0
  * @Date 2023/8/7 18:18
  * @Created by 陈浩东
  */
+@RestController
+@RequestMapping("/patients")
 public class PatientController {
+    @Autowired
+    PatientServiceImpl patientService;
+
+//  查询所有订单
+    @GetMapping("/")
+    @ResponseBody
+    public String getPatientList() {
+        List<Patient> list = patientService.list();
+        return JSONUtil.toJsonPrettyStr(Result.ok(list));
+    }
+
+
 }
