e511da35891a58d325c5b69eb9bca83e0ab66096
weiyin-lu <weiyin2002@outlook.com>
2023-08-08 22:13:56 +0800

    加载mybatis-flex提供的快速配置类

--- src/main/java/online/weiyin/moopoint/config/MybatisFlexConfig.java
+++ src/main/java/online/weiyin/moopoint/config/MybatisFlexConfig.java
@@ -2,6 +2,7 @@ package online.weiyin.moopoint.config;
 
 import com.mybatisflex.core.FlexGlobalConfig;
 import com.mybatisflex.core.audit.AuditManager;
+import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +15,13 @@ import org.springframework.context.annotation.Configuration;
  * @Created by 卢子昂
  */
 @Configuration
-public class MybatisFlexConfig {
+public class MybatisFlexConfig implements MyBatisFlexCustomizer {
 
     private static final Logger logger = LoggerFactory
             .getLogger("mybatis-flex-sql");
 
-    public MybatisFlexConfig() {
+    @Override
+    public void customize(FlexGlobalConfig flexGlobalConfig) {
         //开启审计功能
         AuditManager.setAuditEnable(true);
 
@@ -28,11 +30,9 @@ public class MybatisFlexConfig {
                 logger.info("{},{}ms", auditMessage.getFullSql(), auditMessage.getElapsedTime())
         );
 
-        //逻辑删除标准
-        FlexGlobalConfig globalConfig = FlexGlobalConfig.getDefaultConfig();
         //设置数据库正常时的值
-        globalConfig.setNormalValueOfLogicDelete(1);
+        flexGlobalConfig.setNormalValueOfLogicDelete(1);
         //设置数据已被删除时的值
-        globalConfig.setDeletedValueOfLogicDelete(0);
+        flexGlobalConfig.setDeletedValueOfLogicDelete(0);
     }
 }
